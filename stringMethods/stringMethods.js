82ca53a42a0ca63efc60bbc7d82a8305f09872aa
Hussein-Merrey <hussein96merrey@gmail.com>
2022-08-03 18:12:04 +0300

    Working with JS

---
+++ stringMethods/stringMethods.js
@@ -0,0 +1,23 @@
+//Codin Exercise:18 string methods
+const message = "    TASTE THE RAINBOW!  ";
+const whisper = message.toLowerCase().trim();
+console.log(whisper);
+
+//Coding Exercise:19 string methods
+const word = "skateboard";
+const facialHair = word.slice(5).replace("o", "e");
+console.log(facialHair);
+
+
+//Coding Exercise:20 String Template literal
+const die1 = Math.floor(Math.random() * 6) + 1;
+const die2 = Math.floor(Math.random() * 6) + 1;
+const roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
+console.log(roll);
+
+
+//Coding Exercise:20 Coditional
+let num = prompt("enter a number")
+if (num % 2 === 0) {
+    console.log("even");
+}
